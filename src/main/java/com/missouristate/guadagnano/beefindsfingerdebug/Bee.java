0c674eb0b9d475d0429076f213a7c961a45a2d06
Gua2015 <55575273+Gua2015@users.noreply.github.com>
2019-11-17 23:31:29 -0600

    Add files via upload

---
+++ src/main/java/com/missouristate/guadagnano/beefindsfingerdebug/Bee.java
@@ -0,0 +1,33 @@
+package com.missouristate.guadagnano.beefindsfingerdebug;
+
+public class Bee {
+    private int mX;
+    private int mY;
+    private int mVelocity;
+
+    public void setVelocity(int velocity){
+        mVelocity = velocity;
+    }
+    public int getVelocity(){
+        return mVelocity;
+    }
+
+    public void setX(int x){
+        mX = x;
+    }
+    public void setY(int y){
+        mY = y;
+    }
+    public int getX(){
+        return mX;
+    }
+    public int getY(){
+        return mY;
+    }
+    public void move(int destinationX, int destinationY){
+        int distX = destinationX - mX;
+        int distY = destinationY -  mY;
+        mX += distX / mVelocity;
+        mY += distY / mVelocity;
+    }
+}
